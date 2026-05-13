d2b7d9af5f7dcc08089f7d433ad8d92ef11604ff
asd <dlrudals0324493@gmail.com>
2026-05-13 15:14:35 +0900

    Auto refresh PWA updates

--- src/pwa.js
+++ src/pwa.js
@@ -2,5 +2,11 @@ import { registerSW } from "virtual:pwa-register";
 
 export function registerServiceWorker() {
   if (!import.meta.env.PROD) return;
-  registerSW({ immediate: true });
+  let updateSW;
+  updateSW = registerSW({
+    immediate: true,
+    onNeedRefresh() {
+      updateSW(true);
+    }
+  });
 }
