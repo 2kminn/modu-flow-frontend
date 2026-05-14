14f30f8f75d7c5e76e9023b55ae5d222a29f4cdb
asd <dlrudals0324493@gmail.com>
2026-05-14 12:58:56 +0900

    Remove unused Vercel proxy functions

--- api/api/v1/routines/items/[itemId]/count.js
+++
@@ -1,8 +0,0 @@
-import { proxyToBackend } from "../../../../../_proxy.js";
-
-export default async function handler(req, res) {
-  const itemId = req.query?.itemId;
-  const id = Array.isArray(itemId) ? itemId[0] : itemId;
-  return proxyToBackend(req, res, `api/v1/routines/items/${id}/count`);
-}
-
--- api/auth/signup.js
+++
@@ -1,6 +0,0 @@
-import { proxyToBackend } from "../_proxy.js";
-
-export default async function handler(req, res) {
-  return proxyToBackend(req, res, "auth/signup");
-}
-
